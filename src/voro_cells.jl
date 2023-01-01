e4c2eaec0800ce4bdf4ec2b808ec684fac6205ac
khorrami1 <m.khorrami.70@gmail.com>
2023-01-01 20:12:57 +0100

    updated

--- src/voro_cells.jl
+++ src/voro_cells.jl
@@ -1,12 +1,14 @@
 using Ferrite
+include("Voronoi_Tessellation.jl")
 
 # Generate a grid
-N = 10
+N = 50
 L = 1.0
 dim = 3
 left = zero(Vec{3})
 right = L * ones(Vec{3})
-grid = generate_grid(Tetrahedron, (N, N, N), left, right)
+cell_type = QuadraticTetrahedron 
+grid = generate_grid(Cell{3,20,6}, (N, N, N), left, right)
 
 cells = grid.cells
 nodes = grid.nodes
@@ -24,3 +26,15 @@ centers_cell
 # using FerriteViz
 # import GLMakie
 # FerriteViz.wireframe(grid,markersize=10,strokewidth=2)
+
+# mat_id = make_voro_3D(centers_cell, 5, is_periodic=false)
+
+mat_id = make_voro_3D(centers_cell, 50, is_periodic=true, Lx=L+1e-3, Ly=L+1e-3, Lz=L+1e-3)
+
+# vtk_file = vtk_grid("voro_3D", grid)
+# vtk_cell_data(vtk_file, mat_id, "mat_id")
+# close(vtk_file)
+
+vtk_grid("voro_3D", grid) do vtk_file
+    vtk_cell_data(vtk_file, mat_id, "mat_id")
+end
